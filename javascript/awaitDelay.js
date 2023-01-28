5db23f28046fd76122c9708514b75745b3033bbf
Gerschel <Gerschel_Payne@hotmail.com>
2023-01-27 23:36:35 -0800

    got rid of delay and use MutationObserver

--- javascript/awaitDelay.js
+++
@@ -1,4 +0,0 @@
-class AwaitDelayCallback {
-    static async delay(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
-}
-AwaitDelayCallback.callback = [];
--- javascript/favicon.js
+++ javascript/favicon.js
@@ -1,21 +1,22 @@
 class FaviconHandler {
     static setFavicon() {
-        let root = gradioApp();
-        if (!root) {
-            console.error("gradioApp() returned null, can't find the element.");
-            return;
-        }
-        let link = document.createElement('link');
+        const link = document.createElement('link');
         link.rel = 'icon';
         link.type = 'image/svg+xml';
-        link.href = getComputedStyle(root.querySelector('.icon-container')).backgroundImage.replace(/^url\("|"\)$/g, '');
+        link.href = getComputedStyle(gradioApp().querySelector('.icon-container')).backgroundImage.replace(/^url\("|"\)$/g, '');
         document.getElementsByTagName('head')[0].appendChild(link);
     }
-    static async conditionalTrigger() {
-        while (!gradioApp().querySelector('.icon-container')) {
-            await AwaitDelayCallback.delay(2000);
-        }
-        FaviconHandler.setFavicon();
+    static observeGradioApp() {
+        const observer = new MutationObserver(() => {
+            const iconContainer = gradioApp().querySelector('.icon-container');
+            if (iconContainer) {
+                observer.disconnect();
+                FaviconHandler.setFavicon();
+            }
+        });
+        observer.observe(gradioApp(), { childList: true, subtree: true });
     }
 }
-document.addEventListener("DOMContentLoaded", async function () { await FaviconHandler.conditionalTrigger(); });
+document.addEventListener("DOMContentLoaded", () => {
+    FaviconHandler.observeGradioApp();
+});
